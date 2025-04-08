35efcc3ab7969b41899d743536ced4984da82945
NChironna <chironna.nicola@live.it>
2025-04-08 10:35:58 +0200

    pre routing

---
+++ src/app/content-side-container/content-side/content-side/content-side.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-content-side',
+  imports: [],
+  templateUrl:  'content-side.component.html',
+  styleUrl: 'content-side-component.css',
+  standalone: true,
+})
+export class ContentSideComponent {
+
+}
